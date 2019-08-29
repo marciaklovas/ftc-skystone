d959875247fbc04c9d1cdfc45b9eed3260916023
marciaklovas <marcia.k.lovas@gmail.com>
2019-08-29 19:41:37 -0400

    Updated gradle file settings and created a new TeleOp program.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneTeleOp.java
@@ -0,0 +1,78 @@
+/*
+//  FTC FROGS (#14335) TEAM CODE
+//
+//  TELEOP
+//
+//  Class SkystoneTeleOp
+//
+//  Revisions
+//      08-29-19    Marcia L.   Original
+//
+*/
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="SkystoneTeleOp", group="Linear Opmode")
+public class SkystoneTeleOp extends LinearOpMode {
+
+
+    // declare members
+    //private OurRobot Skystone;
+    private ElapsedTime runtime;
+
+    @Override
+    public void runOpMode() {
+        // 'this' used because opmode is needed to setup hardware
+        //Skystone = new OurRobot(this);
+        runtime = new ElapsedTime(); // not sure how to use this yet
+
+        //Skystone.init();
+
+        sleep(2000);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            //////////////// GAMEPAD 1 (A) ///////////////////
+            //////////////// OMNI DRIVE //////////////////////
+
+            //Skystone.omniDrive.Drive();
+
+            // A held down -> line finding in 4 directions
+            if (gamepad1.a) {
+
+            }
+            if (gamepad2.right_bumper) {
+
+            }
+
+            // B held down -> turning in 2 directions
+            if (gamepad1.b) {
+
+            }
+
+            //////////////// GAMEPAD 2 (B) ///////////////////
+            //////////////// ARM CONTROL //////////////////////
+
+            // Arm controls
+            if (gamepad2.a) {
+
+            }
+
+
+        }
+
+    }
+
+}
+
+
